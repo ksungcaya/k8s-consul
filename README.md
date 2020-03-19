b7fa8ab5f4f959233e9a0cae20f54e38f150cb0b
Kenneth Sungcaya <ksungcaya@gmail.com>
2020-03-19 15:45:38 +0800

    enable HA in consul chart

--- README.md
+++ README.md
@@ -1,12 +1,33 @@
 # Running Consul with Kubernetes
 ## Overview
+Simple Vault management setup using Consul as the backend storage of key-vaule pair configuration.
+
+
 ## Prerequisites
+* [virtualbox](https://www.virtualbox.org/wiki/Downloads)
 * [kubernetes](https://kubernetes.io/docs/tasks/tools/install-kubectl)
-* [helm](https://helm.sh/docs/intro/install/)
+* [helm](https://helm.sh/docs/intro/install/) (need Version 2)
 * [minikube](https://kubernetes.io/docs/tasks/tools/install-minikube/)
 
+
 ## Running the Cluster
+1. Start minkube `minikube start --vm-driver=virtualbox`
 1. From the repository directory, run `kubectl apply -f k8s/`.
-2. Follow instructions: https://learn.hashicorp.com/consul/kubernetes/minikube .
-3. Access consul ui: `minikube service hashicorp-consul-ui`.
-4. Verify if you can get the config by accessing `192.168.99.100:31515/:key`.
+2. Follow instructions: https://learn.hashicorp.com/consul/kubernetes/minikube. Note that all instance of `webapp` have been changed to `app`.
+3. When creating a Kubernetes authentication role, named `app`, that connects the Kubernetes service account name and `app` policy, run the following to match the `serviceAccountName: k8s-consul` that was set in the `deployment.yml` file.
+    ```bash
+    vault write auth/kubernetes/role/app \
+        bound_service_account_names=k8s-consul \
+        bound_service_account_namespaces=default \
+        policies=app \
+        ttl=24h
+    ```
+4. Access consul ui: `minikube service hashicorp-consul-ui`.
+5. Verify if you can get the config by accessing `192.168.99.100:31515/:key`. The sample application assumes that the `config` is in the vault path `secret/data/app/config` where the values are in `json`. 
+
+
+## Further Readings
+* https://www.vaultproject.io/docs/platform/k8s/helm/run/
+* https://learn.hashicorp.com/vault/developer/sm-static-secrets
+* https://learn.hashicorp.com/vault/identity-access-management/iam-policies
+* https://www.vaultproject.io/docs/secrets/consul/ (Option if we are going to access KV's directly from Consul)
--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 const axios = require("axios");
 const express = require("express");
+const fs = require("fs");
+const qs = require("qs");
 
 const app = express();
+const ip = process.env.HOST_IP;
+const vaultUrl = `${process.env.VAULT_ADDR}`;
+
+function getConsulConfig(key) {
+    return axios
+        .get("http://" + ip + ":8500/v1/kv/" + key)
+        .then(result => {
+            const config = result.data[0].Value;
+
+            Promise.resolve(Buffer.from(config, "base64").toString("ascii"));
+        });
+}
+
+function getVaultConfig(auth) {
+    const { data } = auth;
+    const token = data.auth.client_token;
+    const configs = "secret/data/app/config"
+
+    return axios.get(`${vaultUrl}/v1/${configs}`, {
+        headers: { "X-Vault-Token": token }
+    }).then(response => Promise.resolve(response.data));
+}
 
 app.get("/", (req, res) => {
-  res.json({ welcome: "Home" });
+    res.json({ welcome: "Home" });
 });
 
-app.get("/:key", (req, res) => {
-  const ip = process.env.HOST_IP;
-  const key = req.params.key;
-  return axios
-    .get("http://" + ip + ":8500/v1/kv/" + key)
-    .then(result => {
-      const config = result.data[0].Value;
-      const value = Buffer.from(config, "base64").toString("ascii");
+app.get("/:key", async (req, res) => {
+    try {
+        const { key } = req.params;
+        const jwt = fs.readFileSync(process.env.JWT_PATH, "utf-8");
+        const authPath = "auth/kubernetes/login";
 
-      return res.json({ key, value });
-    })
-    .catch(err => {
-      return res.json({ error: err });
-    });
+        const auth = await axios.post(`${vaultUrl}/v1/${authPath}`, {
+            role: "app", // from setup (value was originally "webapp" from the guide)
+            jwt
+        });
+
+        const { data } = await getVaultConfig(auth);
+        let value = null;
+        let version = null;
+
+        if (data.data && data.data[key]) {
+            const { metadata } = data;
+            value = data.data[key];
+            version = metadata.version;
+        }
+
+        res.json({ key, value, version });
+    } catch (error) {
+        console.error(error);
+        res.json(error);
+    }
 });
 
 app.listen(8080, () => {
-  console.log("Listening on port 8080");
+    console.log("Listening on port 8080");
 });
